44c309e9aa6f53b2367f512901ab3e055a451cdc
Thiago Pereira <pereira.tp.thiago@gmail.com>
2024-11-03 21:56:18 -0300

    feat: reorganização das pastas e html do formulario

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Agenda</title>
+</head>
+<body>
+  <?php require('src/controllers/cadastro-usuario.controller.php'); ?>
+</body>
+</html>
---
+++ src/controllers/cadastro-usuario.controller.php
@@ -0,0 +1,2 @@
+<?php
+  require("src/views/cadastro-usuario.view.php");
