2fd60205c29ec2b88d4f08f8c89a19239398b751
MatheusAndreiczuk <matheusandreizuk@gmail.com>
2024-11-04 14:13:15 -0300

    update index

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-require 'src/controllers/login-usuario.controller.php';
+  $rota = explode('/', substr($_SERVER['REQUEST_URI'], 1));
+  $recurso = empty($rota[0]) ? 'agenda' : $rota[0];
+  $controlador = "controllers/$recurso.controller.php";
+  $acao = empty($rota[1]) ? "listar" : $rota[1];
 
-
-exibirLogin();
+  if (file_exists("controllers/${recurso}/${acao}.controller.php")) {
+      require("controllers/${recurso}/${acao}.controller.php");
+  } else {
+      require("controllers/404.controller.php");
+  }
