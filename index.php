74331f1b697fe8b209f474f83e2c3aad438fdb31
Thiago Pereira <pereira.tp.thiago@gmail.com>
2024-11-03 23:12:08 -0300

    feat: criação da tela de cadastro de usuario

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Agenda</title>
+  <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-  <?php require('src/controllers/cadastro-usuario.controller.php'); ?>
+<body class="bg-gray-100 h-screen">
+  <?php require('controllers/cadastro-usuario.controller.php'); ?>
 </body>
 </html>
--- src/controllers/cadastro-usuario.controller.php
+++
@@ -1,2 +0,0 @@
-<?php
-  require("src/views/cadastro-usuario.view.php");
