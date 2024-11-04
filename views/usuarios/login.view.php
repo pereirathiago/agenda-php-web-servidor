338b789b9ccf14837ccf24b6b9d5c53209c5697a
MatheusAndreiczuk <matheusandreizuk@gmail.com>
2024-11-04 15:46:40 -0300

    Update login.view.php

--- views/usuarios/login.view.php
+++ views/usuarios/login.view.php
@@ -1,6 +1,6 @@
 <div class="flex items-center justify-center min-h-screen bg-gray-100">
 <div class="p-6 bg-white rounded-lg shadow-md max-w-md w-full">
-<h1 class="text-3xl font-bold mb-6 text-center text-gray-800">Cadastro de Usuário</h1>
+<h1 class="text-3xl font-bold mb-6 text-center text-gray-800">Login</h1>
     <form action="/controllers/usuarios/login.controller.php?action=autenticarUsuario" method="post">
         <label for="usuario" class="block text-gray-700 font-semibold">Usuário:</label>
         <input type="text" id="usuario" name="usuario" placeholder="Digite o seu nome de usuario" class="w-full p-2 border border-gray-300 rounded mt-1 focus:outline-none focus:ring-2 focus:ring-blue-500" required>
