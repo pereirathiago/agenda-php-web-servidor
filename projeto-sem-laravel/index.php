6decbf018ad90aac427c85bff62ae626ce63fc64
Thiago Pereira <pereira.tp.thiago@gmail.com>
2025-01-26 12:34:13 -0300

    feat: removido projeto antigo, config rota ola mundo

--- projeto-sem-laravel/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php'; //esse autoload é pra conseguir fazer referencia às classes sem precisar importar pelo require
-date_default_timezone_set('America/Sao_Paulo');
-use Pecee\SimpleRouter\SimpleRouter as Router;
-
-require('routes.php');
-
-Router::start();
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\OlaMundoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,6 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/hello-world', function (Request $request) {
-    return response()->json(['message' => 'Hello World!'], 200);
-});
+Route::get('/hello-world', [OlaMundoController::class, 'helloWorld']);
