7cfd54b328248b0fff73cafdb66d35508fd5002b
Thiago Pereira <pereira.tp.thiago@gmail.com>
2025-01-26 14:52:47 -0300

    feat: algumas tabelas banco de dados

---
+++ database/migrations/2025_01_26_174800_create_locais_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('locais', function (Blueprint $table) {
+            $table->id();
+            $table->string('cep');
+            $table->string('endereco');
+            $table->boolean('sem_numero');
+            $table->string('numero')->nullable();
+            $table->string('bairro');
+            $table->string('cidade');
+            $table->string('estado');
+            $table->foreignId('id_usuario')->references('id')->on('usuarios')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('locais');
+    }
+};
